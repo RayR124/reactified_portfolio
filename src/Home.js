c7d1ce146f436fa308bd169dbb6d05fc3abd7039
RayR124 <RealtorRayR@Gmail.com>
2023-02-21 15:34:15 -0800

    added more styling, and added links to works

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
 const Home = () => {
 
     return (
-        <div className="home">
-            <h1 style={{color: "blue", marginBottom: 20, fontSize: 45 }}>Ray's <span style={{color: "#f1356d" }}>Portfolio</span> Web Page</h1>
-            <h2 style={{color: "gray", marginBottom: 15, fontSize: 30 }}>Coder, Father, Husband, Nerd, and Founder of "Raze Games".</h2>
-                <p style={{color: "white", marginBottom: 15, fontSize: 20 }}>Hi, I'm Ray. And this is my Portfolio Web Page.</p>
-                <button onClick={(e) => {
-                    e.preventDefault();
-                    window.location.href='https://www.publicationcoach.com/better-at-following-directions';
-                }} className="button">Don't Click this button.</button>
-            </div>
+        <div className="home" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ color: "blue", marginBottom: 20, fontSize: 45 }}>Ray's <span style={{ color: "#f1356d" }}>Portfolio</span> Web Page</h1>
+            <h2 style={{ color: "gray", marginBottom: 15, fontSize: 30 }}>Coder, Father, Husband, Nerd, and Founder of "Raze Games".</h2>
+            <p style={{ color: "white", marginBottom: 15, fontSize: 20, fontWeight: 700 }}>Hi, I'm Ray. And this is my Portfolio Web Page.</p>
+            <button onClick={(e) => {
+                e.preventDefault();
+                window.location.href = 'https://www.publicationcoach.com/better-at-following-directions';
+            }} className="troll">Don't Click this button.</button>
+        </div>
     );
 }
 
--- src/Works.js
+++ src/Works.js
@@ -1,27 +1,26 @@
 const Works = () => {
     return (
-        <div className="works">
-            <h2 style={{ color: "gray" }}>My Work</h2>
+        <div className="works" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <h2 style={{ color: "gray" }}>My Work:</h2>
             <div>
                 <li style={{ color: "#f1356d" }}>Taylor's Kitchen</li>
-                <ul style={{ color: "white" }}>Taylor's Kitchen is our first Group Project from my BootCamp at UC Berkeley Extension. Our group members were myself, Isayah Durst, Frank Nguyen, Anthony Li ,and Benjamin Ha. It is a meal planning application wherein you can create a meal plan that's tailored to your specific diet preferences. Taylor's Kitchen allows you to customize your diets, cuisines, and food intolerances to your liking to recommend foods and recipes that compliment your tastes and needs.
-
-                    Taylor's Kitchen was built using two different server-side APIs and is a complete front-end solution. Without having access to a back-end, we've leveraged local-storage and third-party data storage solutions to bring this project to life and create something that we found to be challenging, exciting, and useful. </ul>
+                <ul style={{ color: "white", marginBottom: 20 }}>Taylor's Kitchen is our first Group Project from my BootCamp at UC Berkeley Extension. Our group members were myself, <a style={{color: "white" }} href="https://github.com/isayahdurst">Isayah Durst</a>, <a style={{color: "white" }} href="https://github.com/frankmng">Frank Nguyen</a>, <a style={{color: "white" }} href="https://github.com/lunchtimewhee">Anthony Li</a> ,and <a style={{color: "white" }} href="https://github.com/Ahneb">Benjamin Ha</a>. It is a meal planning application wherein you can create a meal plan that's tailored to your specific diet preferences. Taylor's Kitchen allows you to customize your diets, cuisines, and food intolerances to your liking to recommend foods and recipes that compliment your tastes and needs.
+                </ul>
+                <ul style={{ color: "white", marginBottom: 20 }}>Taylor's Kitchen was built using two different server-side APIs and is a complete front-end solution. Without having access to a back-end, we've leveraged local-storage and third-party data storage solutions to bring this project to life and create something that we found to be challenging, exciting, and useful. </ul>
                 <button onClick={(e) => {
                     e.preventDefault();
                     window.location.href = "https://isayahdurst.github.io/taylors-kitchen-meal-prep-app/";
                 }} className="button">View Taylor's Kitchen</button>
                 <li style={{ color: "#f1356d" }}>Media Map</li>
-                <ul style={{ color: "white" }}>Myself, Aidan Espinoza, Braxton Waxdeck, and Charles Cox made this web app for our second project in the UC Berkreley Extension BootCamp. Media Map is a one-stop web-App wherein a user can search for the name of any given movie and the site will return a list of every streaming service that currently has that item available for streaming, and, if necessary, will sort the results based on whether the item is free to stream, free with your subscription, free with ads, charge to rent, or a charge to buy. Users are able to create an account to save their results, as well as create a list of their favorite movies, which the app then uses to populate a suggested list of movies in similar categories.
-
-                    We built this project to alleviate the frustration of finding that special something to watch in today's digital age.
+                <ul style={{ color: "white", marginBottom: 20 }}>Myself, <a style={{color: "white" }} href="https://github.com/aidanespinosa">Aidan Espinoza</a>, <a style={{color: "white" }} href="https://github.com/bbwax">Braxton Waxdeck</a>, and <a style={{color: "white" }} href="https://github.com/CharlesCoxC3">Charles Cox III</a> made this web app for our second project in the UC Berkreley Extension BootCamp. Media Map is a one-stop web-App wherein a user can search for the name of any given movie and the site will return a list of every streaming service that currently has that item available for streaming, and, if necessary, will sort the results based on whether the item is free to stream, free with your subscription, free with ads, charge to rent, or a charge to buy. Users are able to create an account to save their results, as well as create a list of their favorite movies, which the app then uses to populate a suggested list of movies in similar categories.</ul>
+                <ul style={{ color: "white", marginBottom: 20 }}>We built this project to alleviate the frustration of finding that special something to watch in today's digital age.
                     Gone will be the days where clickbait articles and misleading titles give you false or misleading Google search results. This app will tell you straight forward search results, where you can watch your favorite movies with ease and without having to sift through convoluted and fluffed up articles.</ul>
                 <button onClick={(e) => {
                     e.preventDefault();
                     window.location.href = "https://media-map-app.herokuapp.com/";
                 }} className="button">View Media Map</button>
                 <li style={{ color: "#f1356d" }}>Ray's GitHub Repo</li>
-                <ul style={{ color: "white" }}>Click the button below to be taken directly to my GitHub Repository wherein you can see all of the projects I've worked on. Feel free to leave feedback on anything, any and al criticism, be it harsh or constructive, is welcome!</ul>
+                <ul style={{ color: "white", marginBottom: 20 }}>Click the button below to be taken directly to my GitHub Repository wherein you can see all of the projects I've worked on. Feel free to leave feedback on anything, any and al criticism, be it harsh or constructive, is welcome!</ul>
                 <button onClick={(e) => {
                     e.preventDefault();
                     window.location.href = "https://github.com/RayR124";
